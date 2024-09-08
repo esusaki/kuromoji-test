5c202d99f3690616b424d0979462c6d615893c61
esusaki <susaki2002@gmail.com>
2024-09-09 01:15:47 +0900

    foreach tsukau

--- kuromoji.js
+++ kuromoji.js
@@ -9061,5 +9061,5 @@
     kuromoji.builder({ dicPath: "./dict" }).build(function (err, tokenizer) {
         // tokenizer is ready
         var path = tokenizer.tokenize("すもももももももものうち");
-        console.log(path);
+        path.forEach((element)=>{console.log('element.basic_form')});
     });
